e7c5136c15d9b0cee5275f890fdd5de5149ebbe1
Jared Morse <jarcoal@gmail.com>
2013-01-04 13:13:22 -0800

    fix copy/paste bug in DjangoPyPalMixin's cred handling

--- django_pypal/resources.py
+++ django_pypal/resources.py
@@ -11,7 +11,7 @@ class DjangoPyPalMixin(object):
 	Injects the user settings into the resource.
 	"""
 
-	def __init__(self, user_id=SECURITY_USER_ID, security_password=SECURITY_SIGNATURE, security_signature=SECURITY_SIGNATURE, application_id=APPLICATION_ID, debug=settings.DEBUG):
+	def __init__(self, user_id=SECURITY_USER_ID, security_password=SECURITY_PASSWORD, security_signature=SECURITY_SIGNATURE, application_id=APPLICATION_ID, debug=settings.DEBUG):
 		super(DjangoPyPalMixin, self).__init__(user_id, security_password, security_signature, application_id, debug)
 
 #
